3c4a05506417db1caeade2003cb07b14d3831661
Conor Hamilton <conhamil19@gmail.com>
2018-01-11 03:17:03 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,23 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++){
+     if(i === 1){
+  array.push(console.log(`"I am ${i} strange loop."`))}
+  else {
+  array.push(console.log(`"I am ${i} strange loops."`))}}
+  return array
+}
+
+
+function whileLoop(number) {
+  var n = `${number}`
+  while (n>0) {console.log(--n)}
+  if (n === 0) {
+    return 'done'}
+}
+
+function doWhileLoop(array) {
+  do {delete }
+  while(array.length > 0 && maybeTrue())
+  return array
+}
+
